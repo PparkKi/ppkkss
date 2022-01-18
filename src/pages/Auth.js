80f0947141f0a9cf1b899043201f0a0a6049525b
PparkKi <fkawl213@gmail.com>
2022-01-18 15:42:40 +0900

    1.3 login desc modal

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -6,8 +6,28 @@ import Button from "@mui/material/Button";
 import introImg from "images/intro_img.png";
 import Switch from "@mui/material/Switch";
 
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+// 메인 팝업 style
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 450,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 const Auth = () => {
   const [newAccount, setNewAccount] = useState(false);
+  const [open, setOpen] = useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   // 구글, 페이스북 소셜 로그인
   const onSocialClick = async (event) => {
@@ -33,32 +53,55 @@ const Auth = () => {
   const label = { inputProps: { "aria-label": "Switch demo" } };
 
   return (
-    <div id="auth-wrap">
-      <img src={introImg} alt="" className="auth-img" />
-      <div className="auth-container">
-        <AuthForm newAccount={newAccount} />
-        <div className="auth-social">
-          <Button
-            variant="outlined"
-            onClick={onSocialClick}
-            name="google"
-            className="auth-social-button"
-          >
-            google 로그인
-          </Button>
-          <Button
-            variant="outlined"
-            onClick={onSocialClick}
-            name="facebook"
-            className="auth-social-button"
-          >
-            facebook 로그인
-          </Button>
+    <>
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-description">
+              우측 하단 버튼 클릭 시 회원가입이 가능하며 테스트 계정을 사용하셔도 됩니다!<br /><br />
+              id : t1@t1.com<br />
+              pw : asd123
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
+      
+      <div id="auth-wrap">
+        <img src={introImg} alt="" className="auth-img" />
+        <div className="auth-container">
+          <AuthForm newAccount={newAccount} />
+          <div className="auth-social">
+            <Button
+              variant="outlined"
+              onClick={onSocialClick}
+              name="google"
+              className="auth-social-button"
+            >
+              google 로그인
+            </Button>
+            <Button
+              variant="outlined"
+              onClick={onSocialClick}
+              name="facebook"
+              className="auth-social-button"
+            >
+              facebook 로그인
+            </Button>
 
-          <Switch {...label} onClick={toggleAccount} className="auth-toggle" />
+            <Switch
+              {...label}
+              onClick={toggleAccount}
+              className="auth-toggle"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
